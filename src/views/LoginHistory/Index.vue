5c6743683f737b77cce64a149968430a6e74e160
bwpows <bwpows@gmail.com>
2023-02-25 23:41:58 +0800

    feat(登录): 修复登录请求

--- src/views/LoginHistory/Index.vue
+++ src/views/LoginHistory/Index.vue
@@ -18,11 +18,18 @@
             </template>
         </DataTable>
     </v-card>
+    <v-pagination
+        class="mt-3"
+        v-model="current_page"
+        :length="Math.ceil(total/page_count)"
+        :total-visible="7"
+        @update:modelValue="getHistory()"
+    ></v-pagination>
 </template>
 
 <script setup lang="ts">
 
-import {computed, onMounted, ref} from "vue";
+import {computed, onMounted, Ref, ref} from "vue";
 import TimeFun from '@/utils/formatTime'
 import {historyTableHeader, HistoryType, typeEnum} from "@/views/LoginHistory/data";
 import {getAllHistory} from "@/api/loginHistory";
@@ -39,14 +46,21 @@ const computedIp=computed(()=>(item:string)=>{ //计算属性传递参数
 let historyTableData = ref<HistoryType[]>()
 let loading = ref<boolean>(true)
 
+let page_count: Ref<number> = ref(20)
+let current_page: Ref<number> = ref(1)
+let total: Ref<number> = ref(0)
+
 const getHistory = async () => {
-    let res = await getAllHistory()
+    let res = await getAllHistory({
+        current_page: current_page.value,
+        page_count: page_count.value,
+    })
     loading.value = false
     if(res.code == 200){
-        historyTableData.value = res.data
+        historyTableData.value = res.data.data || [];
+        total.value = res.data.total
     }
 }
-
 
 </script>
 
